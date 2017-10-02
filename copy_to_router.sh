d021fe0aff4eb1516686047eed358a3520c4f4ae
Jack Zhou <univerio@gmail.com>
2017-10-01 18:00:18 -0700

    Package for debian.

--- copy_to_router.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# just a helpful script for copying everything over to the router; assumes
-# "router" resolves to your EdgeRouter.
-set -ex
-scp eap_proxy.py router:/config/scripts
-scp eap_proxy.sh router:/config/scripts/post-config.d
-scp eap_tcpdump.sh router:
